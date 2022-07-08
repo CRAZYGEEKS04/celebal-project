267cae30e72681d65364515166e670c748ff23b8
Hritik Maheshwari <60053891+CRAZYGEEKS04@users.noreply.github.com>
2022-07-09 01:55:24 +0530

    Add files via upload

---
+++ src/Components/Main.js
@@ -0,0 +1,41 @@
+import react ,{useState}from "react";
+import Card from "./Card";
+import axios from "axios";
+const Main=()=>{
+    const [search,setSearch]=useState("");
+    const [bookData,setData]=useState([]);
+    const searchBook=(evt)=>{
+        if(evt.key==="Enter")
+        {
+            axios.get('https://www.googleapis.com/books/v1/volumes?q='+search+'&key=AIzaSyA6SaT23KNiiA6DnUfUQTvFeyAcQEkwnSU')
+            .then(res=>setData(res.data.items))
+            .catch(err=>console.log(err))
+        }
+    }
+    return(
+        <>
+            <div className="header">
+                <div className="row1">
+                    <h1>Nothing is pleasanter than <br/> exploring a library.</h1>
+                </div>
+                <div className="row2">
+                    <h2>Discover your next great read</h2>
+                    <div className="search">
+                        <input type="text" placeholder="Enter Your Book Name"
+                        value={search} onChange={e=>setSearch(e.target.value)}
+                        onKeyPress={searchBook}/>
+                        <button id="search-button" type="button" className="btn btn-primary"></button>
+                    </div>
+                    <img src="../images/bg2.jpg" alt="" />
+                </div>
+            </div>
+
+            <div className="container">
+              {
+                    <Card book={bookData}/>
+              }  
+            </div>
+        </>
+    )
+}
+export default Main;
